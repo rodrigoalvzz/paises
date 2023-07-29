7cd61ce67acc608cd4c5734100410fe76737eb34
rodrigoalvzz <123877334+rodrigoalvzz@users.noreply.github.com>
2023-07-29 08:59:38 -0300

    Add files via upload

--- Entregable 1- Carga datos API.sql
+++ Entregable 1- Carga datos API.sql
@@ -1,3 +1,8 @@
-SQL
-create database PAISES
+-- Crea una tabla llamada "country_data"
+CREATE TABLE country_data (
+    country_name VARCHAR(255),
+    capital VARCHAR(255),
+    population INT,
+    region VARCHAR(255)
+);
 
